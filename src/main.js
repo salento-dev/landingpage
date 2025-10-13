2e685d10b95cb4423da2fd307a8476c95f25c479
Fabio D'Urso <fabio.durso.89@gmail.com>
2025-10-13 16:19:53 +0200

    fix: aggiornata la branch di attivazione del workflow di deploy a 'feature/vite' e corretta l'istanza di Typed.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './style.css'
 import Typed from 'typed.js';
 
 
-new Typed('.typed-code', {
+const typed = new Typed('.typed-code', {
   strings: [
     `console.log('Benvenuto in Salento.dev!');`,
     'const salento = "passione + condivisione";\n\tfunction condividi(idea) {\n \t\treturn idea + " ❤️";\n \t}\n condividi("conoscenza");',
